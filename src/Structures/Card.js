b53965e6eb804259105c9e1f41f6d4182e8f1e15
Ghost <92710767+GhostRider788@users.noreply.github.com>
2023-04-14 21:31:00 +0530

    Update Card.js

--- src/Structures/Card.js
+++ src/Structures/Card.js
@@ -42,7 +42,7 @@ module.exports = class Card {
   }
 
   get URL() {
-    return `https://raw.githubusercontent.com/Ratismal/UNO/master/cards/${this.color || ''}${this.id}.png`;
+    return `https://raw.githubusercontent.com/GhostRider788/cards/main/cards/${this.color || ''}${this.id}.png`;
   }
 
   get value() {
